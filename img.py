9aecbe9f245fd3be991dab0ab1334303c1f5a994
Buteux <twbb2@leicester.ac.uk>
2023-08-30 12:27:12 +0100

    implementation complete. seems to work well with images 1 and 2. need to try on the rest

--- img.py
+++ img.py
@@ -19,7 +19,7 @@ import utils
 
 
 # loading in the image
-def img2codes(file):
+def imgSetUp(file):
 
     """ loading steps """
     # load the file using fits
@@ -85,7 +85,7 @@ def img2codes(file):
     # show the image
     # apply a gaussian blur to the image
     image = cv2.GaussianBlur(image, (5, 5), 0)
-    """
+    
 
     fig, ax = plt.subplots(2, 2, figsize=(15, 5))
     ax[0][0].imshow(initial_image, cmap='gray')
@@ -97,7 +97,7 @@ def img2codes(file):
     ax[1][1].imshow(image_median, cmap='gray')
     ax[1][1].set_title('Median Image')
     plt.show()
-    """
+    
 
 
     """ setting up detector """
@@ -125,76 +125,57 @@ def img2codes(file):
     corners = [[keypoint.pt[0], keypoint.pt[1]] for keypoint in keypoints]
     sizes = [keypoint.size for keypoint in keypoints]
 
-    coords = sorted(zip(sizes, corners), reverse=True)[:30]
+    coords = sorted(zip(sizes, corners), reverse=True)
     sizes, corners = map(list, zip(*coords))
+
 
     # creating img_data dataframe containing all stars in image cols = ['x', 'y', 'size']
     img_data = pd.DataFrame(corners, columns=['x', 'y'])    
     img_data['size'] = sizes
     img_data = img_data.sort_values(by=['size'], ascending=False)
     img_data['count'] = 0
+    img_tree = cKDTree(img_data[['x', 'y']])
+
+    return img_data, image_size, img_tree, image
 
 
+# creating progressive function
+def generateQuads(N, img_data, image_size,img_quads, img_codes):
+    current_data = img_data.head(N).copy()
+    star = N-1
+    # find all neigbors of the star that are within 0.35 * image_size
+    tree = cKDTree(current_data[['x', 'y']])
+    neighbors = tree.query_ball_point(current_data.iloc[star][['x', 'y']], 0.35*image_size)
+    # drop the star from the neighbors
+    neighbors.remove(star)
+    neighbors = sorted(neighbors)
 
-    # creating img_quads list of quads in image, in data.index format
+    # check if there are at least 4 neighbors
+    if len(neighbors) < 3:
+        return img_quads, img_codes
+    for combination in combinations(neighbors,3):
+        quad = [star, *combination]
+
+        # check if the quad is already in the list
+        if tuple(sorted(quad)) in img_quads:
+            continue
+        # check if the scale of the quad is within 0.35 and 0.25 of the image size
+        A,B,C,D,scale = utils.findABCD(current_data.loc[quad,['x','y']].values)
+        if (scale > 0.35*image_size) or (scale < 0.25*image_size):
+
+            continue
+        # check if C and D lie in the within the A,B,MidAB circle
+        midpoint = np.mean([A, B], axis=0)
+        # check if C and D are within 0.5*AB of the midpoint
+        distance = np.linalg.norm(np.subtract(midpoint,A))
+        if (np.linalg.norm(np.subtract(C,midpoint)) > distance) or (np.linalg.norm(np.subtract(D,midpoint)) > distance):
+            continue
+        # append the quad to the list
+        img_quads.append(tuple(sorted(quad)))
+        img_codes.append(utils.hashcode(current_data.loc[quad,['x','y']].values))
     
-    img_quads = []
-    img_codes = []
-     #reset the data index
-    img_data = img_data.reset_index(drop=True)
-    tree = cKDTree(img_data[['x', 'y']].values)
-
-    run_img_pass(img_data, image_size, tree, 7, img_quads, img_codes, quad_scale=0.35)
-
-
-    return img_data, img_quads, img_codes, np.shape(image), image, target, initial_image # shape required for finding centre of image in sol.py
+    print('image quads: ', len(img_quads))
+    return img_quads, img_codes
 
 
 
-def run_img_pass(img_data, image_size, tree, cnt, img_quads, img_codes, quad_scale):
-    # itterating through the corners to create quads
-    for star in img_data.index.to_list():
-        if img_data.loc[star, 'count'] >= cnt:
-            continue
-        # find the neighbouring stars within 0.35*image_size
-        neighbours = tree.query_ball_point(img_data.loc[star, ['x', 'y']].values, 0.35*image_size)
-        try:
-            # remove the star itself from the neighbours
-            neighbours.remove(star)
-        except:
-            print('star {} not in neighbours' .format(star))
-            continue
-        # itterate through the neighbours to create quads
-        for combination in combinations(neighbours, 3):
-            quad = [star,*combination]
-
-            # check if any of the stars in the quad have already been used 3 times
-            if img_data.loc[quad, 'count'].max() >= cnt:
-                continue
-            # sort the quad
-            quad = tuple(sorted(quad))
-            # check if the quad is already in the list
-            if quad in img_quads:
-                continue # if it is, skip to the next combination
-
-            # check the scale of the quad
-            A, B, C, D, scale = utils.findABCD(img_data.loc[quad, ['x', 'y']].values)
-            # if the scale is not between 0.25 and 0.35, skip to the next combination
-            if (scale < quad_scale-0.1 * image_size) or (scale > quad_scale * image_size):
-                continue
-
-            # find the midpoint of AB
-            midpoint = np.mean([A, B], axis=0)
-            # check if C and D are within 0.5*AB of the midpoint
-            distance = np.linalg.norm(np.subtract(midpoint,A))
-            if (np.linalg.norm(np.subtract(C,midpoint)) > distance) or (np.linalg.norm(np.subtract(D,midpoint)) > distance):
-                continue
-
-
-            # add the quad to the list
-            img_quads.append(quad)
-            # convert the quad into a hashcode
-            img_codes.append(utils.hashcode(img_data.loc[quad, ['x', 'y']].values))
-
-            # add 1 to the count of each star in the quad
-            img_data.loc[list(quad), 'count'] += 1
--- sol.py
+++ sol.py
@@ -28,14 +28,133 @@ print('------------------')
 cat_data = pd.read_pickle('cat_data.pkl')
 cat_quads = np.load('quads.npy', allow_pickle=True)
 cat_codes = np.load('hashcodes.npy', allow_pickle=True)
+cat_tree = cKDTree(cat_codes)
+cat_tree_cartesian = cKDTree(cat_data[['x','y','z']].values)
 print('catalogue data loaded successfully')
 print(len(cat_quads), 'catalogue quads')
 
 # creating img_data, img_quads, img_codes
-file = 'test_sets/60arcmin1.fits'
-img_data, img_quads, img_codes, img_shape, image, target, initial_image = img.img2codes(file)
-print('image processed successfully')
-print(len(img_quads), 'image quads')
+file = 'test_sets/60arcmin2.fits'
+img_data, image_size, img_tree, image  = img.imgSetUp(file)
+img_quads = []
+img_codes = []
+N_max = 20
+w = []
+found = False
+for N in range(4,N_max):
+    if found == True:
+        break
+    img_quads, img_codes = img.generateQuads(N, img_data, image_size, img_quads, img_codes)
+    # check if their are any quads
+    if len(img_quads) == 0:
+        continue
+    # check if the quads have any close matches in the index (cat_codes)
+    distances, indices = cat_tree.query(img_codes, k=1)
+
+    # return the indices of any hashcodes that have a distance < 0.1
+    rows = np.where(distances < 0.08)
+
+    # find the indices of the matching hashcodes
+    matching_cat_indices = indices[rows]
+    matching_img_indices = rows
+
+    for i in range(len(matching_cat_indices)):
+        cat_index = matching_cat_indices[i]
+        img_index = matching_img_indices[0][i]
+        # convert the matching hashcodes into quads 
+        cat_quad = cat_quads[cat_index]
+        img_quad = img_quads[img_index]
+
+        # convert the quads into a list of stars
+        cat_stars = cat_data.loc[list(cat_quad),['RA','DE']].values
+        img_stars = img_data.loc[list(img_quad),['x','y']].values
+
+
+        # finding the ref and val stars
+        img_coords, _, _ = utils.sortABCD(img_stars)
+        cat_coords, _, _ = utils.sortABCD(cat_stars)
+        img_A = img_coords[0]
+        img_B = img_coords[1]
+        cat_A = cat_coords[0]
+        cat_B = cat_coords[1]
+
+        # create a WCS object
+        w = WCS(naxis=2)
+        w.wcs.crpix = [img_A[0], img_A[1]]
+        w.wcs.crval = [cat_A[0], cat_A[1]]
+        w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+
+        # calculate the scale
+        img_scale = np.linalg.norm(np.subtract(img_B, img_A))
+        cat_scale = np.linalg.norm(np.subtract(cat_B, cat_A))
+        scale = cat_scale / img_scale # degrees per pixel
+
+        # calculate the rotation matrix
+        img_AB = np.subtract(img_B, img_A)
+        cat_AB = np.subtract(cat_B, cat_A)
+        theta = np.arccos(np.dot(img_AB, cat_AB) / (np.linalg.norm(img_AB) * np.linalg.norm(cat_AB)))
+
+        # calculate the CD matrix
+        cd11 = scale * np.cos(theta)
+        cd12 = -scale * np.sin(theta)
+        cd21 = scale * np.sin(theta)
+        cd22 = scale * np.cos(theta)
+        w.wcs.cd = np.array([[cd11, cd12], [cd21, cd22]])
+
+        # test the WCS object against the image
+        # query the quad centre coordinates against the cat_tree_cartesian
+        cat_centroid = utils.centroid(cat_data.loc[list(cat_quad),['x','y','z']].values)
+        ind = cat_tree_cartesian.query_ball_point(cat_centroid, r = 2 * np.sin(np.radians(0.5)/2))
+        # convert the indices into a list of stars
+        cat_test = cat_data.loc[list(cat_data.iloc[ind].index),['RA','DE']].values
+        print('cat_test\n', cat_test)
+
+        # convert the list of stars into pixel coordinates
+        cat_pix = w.all_world2pix(cat_test,1)
+
+        # query the cat_pix against the img_tree
+        ind2 = img_tree.query_ball_point(cat_pix, r = 10)
+        print('ind2\n', ind2)
+
+
+
+        # find the number of non-empty lists
+        non_empty = np.count_nonzero(ind2)
+        if non_empty >= 4:
+            # find the RA and DE of centre of the image
+            width = np.shape(image)[0]
+            height = np.shape(image)[1]
+            centre = w.all_pix2world(width/2, height/2, 1)
+
+            cd_matrix = w.wcs.cd
+
+            # Using arctan2 to get the angle
+            roll_angle_rad = np.arctan2(-cd_matrix[0, 1], cd_matrix[1, 1])
+
+            # Convert the angle to degrees
+            roll_angle_deg = np.rad2deg(roll_angle_rad)
+
+            # Adjust the angle to be in the range [0, 360]
+            roll_angle_deg = (roll_angle_deg + 360) % 360
+            print(img_quad)
+            print('centre: ', centre)
+            print('roll (degrees E of N): ', roll_angle_deg)
+            print('-------------------------')
+            found = True
+            break
+        else:
+            w = None
+            print('-------------------------')
+            continue
+
+
+if w == None:
+    print('no WCS found')
+
+
+
+
+"""
 
 # convert img_codes and cat_codes into a cKDTree
 cat_tree = cKDTree(cat_codes)
@@ -204,3 +323,4 @@ plt.tight_layout()
 plt.show()
 
 
+"""
